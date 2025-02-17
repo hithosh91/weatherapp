a4b99fddbe1a4ed9ba2a1c3572680e7c0817f8bd
hithosh kota <hithoshkota91@yahoo.com>
2025-02-17 10:29:41 +0000

    Fix line ending issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,91 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+type WeatherApp = {
+  name: string;
+  description: string;
+  temp_min: number;
+  temp_max: number;
+  humidity: number;
+  main: string;
+};
+
+import { useState } from "react";
+
+const Page = () => {
+  const [city, setCity] = useState("");
+  const [data, setData] = useState<WeatherApp>({
+    name: "",
+    description: "",
+    temp_min: 0,
+    temp_max: 0,
+    humidity: 0,
+    main: "",
+  });
+  const apikey = "f70aec4475c51f4ad1a2017634dd5bbe";
+  console.log(data);
+
+  const fetchData = async () => {
+    if (!city) return;
+    try {
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apikey}`
+      );
+      const weatherData = await response.json();
+      const formattedWeather: WeatherApp = {
+        name: weatherData.name,
+        description: weatherData.weather[0].description,
+        temp_min: weatherData.main.temp_min,
+        temp_max: weatherData.main.temp_max,
+        humidity: weatherData.main.humidity,
+        main: weatherData.weather[0].main,
+      };
+      setData(formattedWeather);
+    } catch (error) {
+      console.error("Error fetching weather data:", error);
+    }
+  };
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+    <div className="w-full h-screen bg-gradient-to-r from-red-500 to-yellow-500 flex flex-col justify-center items-center p-4 animate-fadeIn">
+      <h1 className="text-3xl font-bold text-white mb-4">Weather App</h1>
+      <div className="w-full max-w-md bg-white rounded-lg shadow-lg flex flex-col justify-between items-center p-6 transition-all duration-500 ease-in-out transform hover:scale-105">
+        <input
+          type="text"
+          placeholder="Enter city name"
+          className="w-full h-12 rounded-lg border-2 border-gray-300 text-center focus:ring-2 focus:ring-blue-400 focus:outline-none mb-4"
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
         />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+        <button
+          className="w-full bg-blue-500 text-white font-semibold py-2 rounded-lg hover:bg-blue-600 transition-all duration-300"
+          onClick={fetchData}
+        >
+          Search
+        </button>
+        <div className="w-full bg-sky-300 rounded-lg p-5 flex flex-col justify-center items-center mt-4 shadow-md">
+          <h1 className="text-2xl font-mono text-black">{data.name}</h1>
+          <h2 className="text-lg text-orange-700">{data.description}</h2>
+          <div className="flex flex-col sm:flex-row justify-between items-center gap-4 mt-2">
+            <h3 className="text-lg">
+              Temp Min:{" "}
+              <span className="text-2xl text-gray-500 p-2">
+                {data.temp_min}°C
+              </span>
+            </h3>
+            <h3 className="text-lg">
+              Temp Max:{" "}
+              <span className="text-2xl text-gray-500 p-2">
+                {data.temp_max}°C
+              </span>
+            </h3>
+          </div>
+          <h3 className="text-2xl text-blue-700 font-semibold mt-2">
+            {data.main}
+          </h3>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
     </div>
   );
-}
+};
+
+export default Page;
